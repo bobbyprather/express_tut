f84630b243b00489e7312d77e8e8fc6031f0668d
Bobby Prather <bobby.prather@yahoo.com>
2019-04-29 20:42:59 -0500

    Update endpoint

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 const express = require('express');
 const app = express();
-//Hello World 
+app.use(express.json());
+
+const courses = [
+    {id:1, name: "course1"},
+    {id:2, name: "course2"},
+    {id:3, name: "course3"}
+];
 app.get('/',(req,resp)=>{
     resp.send('Hello World');
 });
 
 app.get('/api/courses',(req,resp)=>{
-    resp.send([1,2,3]);
+    resp.send(courses);
 });
 
 app.get('/api/courses/:id',(req,res)=>{
-    res.send(req.params.id);
+    const course =  courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('The course with the given was not found')
+    res.send(course)
 });
 
+
+app.post('/api/courses',(req,res)=>{
+    if(!req.body.name || req.body.name < 3 ){
+        // 400 Bad Request
+        res.status(400).send("Name is requried and should be minimum characthers")
+        return;
+    }
+    
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+
+    courses.push(course);
+    res.send(course);
+});
 //PORT 
 const port = process.env.PORT || 3000;
 app.listen(port,()=>console.log(`Listening on port ${port}.....` ));
